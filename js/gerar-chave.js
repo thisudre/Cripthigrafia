f981f0d3e2a6e619df170e5ab5f5342b934b1ad3
Thiago Sudré Cezarino <thiagoty@gmail.com>
2019-06-05 14:20:51 -0300

    Correção de bugs

--- js/gerar-chave.js
+++ js/gerar-chave.js
@@ -1,11 +1,36 @@
-var form = document.querySelector("#form-generate")
 var btnGerarChave = document.querySelector("#generate-key")
 
 btnGerarChave.addEventListener("click", function(){
-    console.log("clicou no botao")
-    keyRSA = obterChavePeloForm(form)
+    event.preventDefault()
 
+    var form = document.querySelector("#form-generate")
+    
+    var primeNumbers = obterChavePeloForm(form)
+    
+    var erros = validarCampos(primeNumbers)
+    var mensagensErro = document.querySelector("#erros-geracao")
+
+    if (erros.length > 0)
+    {
+        exibirErros(erros)
+        return
+    }
+    mensagensErro.innerHTML = ''
+
+    var keyRSA = criarChave(primeNumbers)
+
+
+    erros = validarChave(keyRSA)
+    if (erros.length > 0)
+    {
+        exibirErros(erros)
+        return
+    }
+    mensagensErro.innerHTML = ''
+    
     mostrarChave(keyRSA)
+
+    form.reset()
 })
 
 function obterChavePeloForm(form)
@@ -13,45 +38,50 @@ function obterChavePeloForm(form)
     var primeNum1 = form.querySelector("#prime-number1").value
     var primeNum2 = form.querySelector("#prime-number2").value
     var E = form.querySelector("#prime-number3").value
-    // validarCampos(primeNum1, primeNum2, E)
-    var erros = validarCampos(primeNum1, primeNum2, E)
-    if (erros.len>0)
-    {
-        exibirErros(erros)
-        return
+
+    primeNumbers = {
+        prime1 : primeNum1,
+        prime2 : primeNum2,
+        E : E
     }
 
-    var mensagensErro = document.querySelector("#erros-geracao")
-    mensagensErro.innerHTML = ''
-    var N = calcularN(primeNum1, primeNum2)
-    var phyN = calcularPhyN(primeNum1, primeNum2)
-    console.log("obteve os dados")
-    var D = calcularD(E, phyN)
-    console.log("calculou D")
-    erros = validarChave(E, N, phyN, D)
-    if (erros.len>0)
-    {
-        exibirErros(erros)
-        return
-    }
-
-    mensagensErro.innerHTML = ''
-
-    var keyRSA = montarChave(E, N, phyN, D)
-
-    return keyRSA
+    return primeNumbers
 }
+
+function criarChave(primeNumbers)
+{
+    var N = calcularN(primeNumbers.prime1, primeNumbers.prime2)
+    var phyN = calcularPhyN(primeNumbers.prime1, primeNumbers.prime2)
+    var D = calcularD(primeNumbers.E, phyN)
+
+    return (montarChave(primeNumbers.E, N, phyN, D))
+}
+
+function montarChave(E, N, phyN, D)
+{
+    var KeyRSA = 
+    {
+        N : N,
+        phyN : phyN,
+        E : E,
+        D : D
+    }
+
+    return KeyRSA
+}
+
 
 function calcularD(E, phyN)
 {
     D = 1
-    while((D*E) % phyN != 1)
+    while((D * E) % phyN != 0)
     {
         D = D+1
     }
-
+    console.log("Valor de D: " + D)
     return D;
 }
+
 
 function calcularN(primeNum1, primeNum2)
 {
@@ -69,73 +99,78 @@ function mostrarChave(keyRSA)
     textSpace.textContent = keyRSA.D
 }
 
-function montarChave(E, N, phyN, D)
-{
-    var KeyRSA = 
-    {
-        N : N,
-        phyN : phyN,
-        E : E,
-        D : D
-    }
 
-    return KeyRSA
-}
-
-function validarCampos(primeNum1, primeNum2, E)
+function validarCampos(primeNumbers)
 {
     erros = []
-    if(!isNaN(primeNum1) && !isNaN(primeNum2) && !isNaN(E))
+    if(!isNaN(primeNumbers.prime1) && !isNaN(primeNumbers.prime2) && !isNaN(primeNumbers.E) && primeNumbers.prime1 != '' && primeNumbers.prime2 != '' && primeNumbers.E != '')
     {
         // Erros possiveis:
         // os numeros nao serem primos
-        if(verificarPrimo(primeNum1))
+        if(!verificarPrimo(primeNumbers.prime1))
         {
             erros.push("Primeiro número não é primo")
+            console.log(erros[erros.len - 1])
         }
 
-        if(verificarPrimo(primeNum2))
+        if(!verificarPrimo(primeNumbers.prime2))
         {
             erros.push("Segundo número não é primo")
+            console.log(erros[erros.len - 1])
         }
         
-        if(verificarPrimo(E))
+        if(!verificarPrimo(primeNumbers.E))
         {
             erros.push("Terceiro número não é primo")
+            console.log(erros[erros.len - 1])
         }
         
-        if(primeNum1 == primeNum2 || primeNum1 == E || primeNum2 == E)
+        if(primeNumbers.prime1 == primeNumbers.prime2 || primeNumbers.prime1 == primeNumbers.E || primeNumbers.prime2 == primeNumbers.E)
         {
             erros.push("Os números não podem ser iguais")
+            console.log(erros[erros.len - 1])
         }
     }
     else
     {
-        erros.push("Todos os campos precisam ser números!")
+        erros.push("Todos os campos precisam ser preenchidos por números!")
+        console.log(erros[erros.len - 1])
     }
 
     return erros
 }
 
-function validarChave(E, N, phyN, D)
+function validarChave(keyRSA)
 {
     erros = []
     // Erros possiveis:
     // os numeros serem muito pequenos
-    if(N<=127)
+    if(keyRSA.N<=127)
     {
         erros.push("Primeiro ou segundo número precisam ser maiores")
+        console.log(erros[erros.len - 1])
     }
     // O numero presente em E não pode dividir N
-    if(E % N == 1)
+    if(keyRSA.E % keyRSA.N == 1)
     {
         erros.push("O terceiro número precisa ser diferente")
+        console.log(erros[erros.len - 1])
     }
+
+    return erros
 }
 
-function verificarPrimo(num) {
-    for (var i = 2; i < num; i++){
-        if (num % i == 0) return false
+function verificarPrimo(num) 
+{
+    var i = 0
+
+    for (i = 2; i<= Math.sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            console.log("Dividido por: " + i)
+            return false
+        }
     }
     return true
 }
@@ -150,4 +185,9 @@ function exibirErros(erros)
         liErro.textContent = erro
         ulErros.appendChild(liErro)
     })
+}
+
+function limpar(campo)
+{
+    campo.textContent = ''
 }
